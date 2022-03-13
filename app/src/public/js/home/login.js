0184d2db04e97a37748e407cd91ccf57bee4bc19
Suyeon04 <s2026@e-mirim.hs.kr>
2022-03-13 16:04:33 +0900

    Dom이용

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,3 +1,18 @@
 "use strict";
 
-console.log("hello")
+const id = document.querySelector("#id"), 
+pword = document.querySelector("#psword"),
+loginbutton = document.querySelector("button");
+
+loginbutton.addEventListener("click", login);
+
+function login(){
+    const req = {
+        id  : id.value,
+        psword : pword.value,
+    }
+ console.log(req);
+}
+
+
+

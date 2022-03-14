c2209583a459ff23e4c9a445a5b5a42011cbab7a
Suyeon04 <s2026@e-mirim.hs.kr>
2022-03-14 11:31:16 +0900

    .

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,14 +1,39 @@
 "use strict";
 
-const home = (req, res) => {
-  res.render("home/index");
+const users = {
+  id:["clap", "kite"],
+  psword:["1234","1357"],
 };
+const process={
+  login : (req, res)=>{
+    const id = req.body.id, 
+    psword = req.body.psword;
 
-const login = (req, res) => {
+    if(users.id.includes(id)){
+      const idx = users.id.indexOf(id);
+      if(users.psword[idx]===psword){
+        return res.json({
+          success: true,
+        });
+      }
+    }
+    return res.json({
+      success: false,
+      msg:"로그인에 실패함",
+    });
+  }
+}
+
+const output ={
+home : (req, res) => {
+  res.render("home/index");
+},
+login : (req, res) => {
   res.render("home/login");
+},
 };
 
 module.exports = {
-  home,
-  login,
+  output,
+  process,
 };

d90cac231c6c99065dd16d9000a12a8bd748c690
Suyeon04 <s2026@e-mirim.hs.kr>
2022-03-14 22:34:36 +0900

    ..

---
+++ app/src/public/js/home/register.js
@@ -0,0 +1,38 @@
+"use strict";
+
+const id = document.querySelector("#id"),
+idname = document.querySelector("#name"),
+psword = document.querySelector("#psword"),
+confirmPsword = document.querySelector("#confirm-psword"),
+registerbtn = document.querySelector("#button");
+
+registerbtn.addEventListener("click", register);
+
+function register(){
+    const req = {
+        id  : id.value,
+        name : idname.value,
+        psword : psword.value,
+        confirmPsword: confirmPsword.value
+    }
+    fetch("/register", {
+        method : "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(req),
+    }).then((res)=> res.json())
+    .then((res)=>{
+        if(res.success){
+            location.href="/login";
+        }else{
+            alert(res.msg);
+        }
+    }).catch((err)=>{
+        console.error("회원가입 중 에러 발생");
+    });
+
+}
+
+
+
